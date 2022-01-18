337193ea182bed6b14079bf143b53435360b4c36
Lukas Klappert <lklappert.mmt-b2013@fh-salzburg.ac.at>
2022-01-18 19:45:57 +0100

    test

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(window.data);
+  console.log(window.data2);
+
+  const modifiedData = window.data.map(x => x*2);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hier kannst du dann über <code>window</code> auf die Daten zugreifen, die du in der index.html definiert hast:
+
+      <pre>{JSON.stringify(window.data)}</pre>
+      <pre>{JSON.stringify(window.data2)}</pre>
+      <pre>{JSON.stringify(modifiedData)}</pre>
+
     </div>
   );
 }
